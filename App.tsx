54eee65a7eb17c38e3001c14317e407fc03d5038
vishalbrdr <biradarvishal290@gmail.com>
2024-03-08 23:57:13 +0530

    Update App component

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const App = () => {
     'https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=f425736747d567d4b635e65bef504761',
   );
   return (
-    <View style={{flex: 1, padding: 24}}>
+    <View style={{flex: 1, padding: 20}}>
       <Text style={{fontSize: 24}}>Hello Welcome to my App</Text>
       {isLoading ? (
         <ActivityIndicator />
